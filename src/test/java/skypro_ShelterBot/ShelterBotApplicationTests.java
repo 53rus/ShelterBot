f9adb196a3f7cb1377b152e2e7649530018b7346
53rus <53rus@mail.ru>
2024-08-27 02:29:49 +0300

    Initial commit

---
+++ src/test/java/skypro_ShelterBot/ShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro_ShelterBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
